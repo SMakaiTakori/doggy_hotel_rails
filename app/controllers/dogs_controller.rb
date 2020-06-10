d0f31e497fac1c7341e6993b98b53213da22d145
SMakaiTakori <makait87@icloud.com>
2020-06-09 23:31:09 -0400

    installed bcrypt and added validations for dog object

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -5,11 +5,18 @@ class DogsController < ApplicationController
     end
 
     def create
-        
-
+        @dog = Dog.new(dog_params)
+        @dog.save
+        redirect_to dog_path(@dog)
     
     end
 
+    private 
+
+    def dog_params
+        params.require(:dog).permit(:name, :age, :breed, :owner, :phone, :biography, :password_digest, :password_confirmation)
+    
+    end
 
 
 end
--- app/models/dog.rb
+++ app/models/dog.rb
@@ -3,6 +3,11 @@ class Dog < ApplicationRecord
     has_many :reservations
     has_many :hotels, through: :reservations
 
+    validates :name, presence: true
+    validates :email, presence: true
+    validates :password, presence: true
+    validates :password, confirmation: { case_sensitive: true }
+
 
     has_secure_password
 
