98e1ff1d9a2e8baf50a7a1091c04b116b0e9fbca
SMakaiTakori <makait87@icloud.com>
2020-06-10 22:06:29 -0400

    created helper methods and before action in users controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,17 @@
 class ApplicationController < ActionController::Base
+    helper_methods :current_user, :logged_in
 
     def welcome
     end
+
+    def current_user
+        User.find_by(id: session[:user_id])    
+    end
+
+    def logged_in
+        current_user ? current_user : redirect_to root_path    
+    end
+
+
 
 end
--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,4 +1,6 @@
 class DogsController < ApplicationController
+    
+    before_action :logged_in?, except: [:new, :create, :welcome]
 
     def new
         @dog = Dog.new        
