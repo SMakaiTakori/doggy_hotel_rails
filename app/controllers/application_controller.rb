482b2216d2abb084dfda1f872c9d8a313c415c6a
SMakaiTakori <makait87@icloud.com>
2020-06-12 12:11:44 -0400

    updated reservation#index and corresponding link and view, updated application#welcome action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
     helper_method :current_user, :logged_in
 
     def welcome
+        @dog = current_user
     end
 
     def current_user
@@ -9,15 +10,7 @@ class ApplicationController < ActionController::Base
     end
 
     def logged_in
-        # if current_user 
-        #      current_user
-        # else
-        #     redirect_to root_path
-        # end    
-
         !!session[:user_id]  
     end
-
-
 
 end
--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -4,6 +4,7 @@ class ReservationsController < ApplicationController
 
     def index
         @reservation= Reservation.all
+        @dog = Dog.find_by(id: session[:id])
     end
 
     def new         
@@ -28,7 +29,7 @@ class ReservationsController < ApplicationController
 
     def show
         @reservation = Reservation.find_by(id: params[:id])
-        @dog = Dog.find_by(id:session[:user_id])
+        @dog = Dog.find_by(id: session[:user_id])
     end
 
     def edit
