d29a6418def1e3cc608ab04f5edd442a16a121f2
SMakaiTakori <makait87@icloud.com>
2021-01-04 14:48:08 -0500

    switched googleauth token to show client id

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  # provider :google_oauth2, '620729860798-90ghhjafgbuvb5s3t8beoo1odir8b6hs.apps.googleusercontent.com', 'VxtlR9ypcVt75KMinPUZpbEP', skip_jwt: true
-  provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"],ENV["GOOGLE_CLIENT_SECRET"], skip_jwt: true
+  provider :google_oauth2, '620729860798-90ghhjafgbuvb5s3t8beoo1odir8b6hs.apps.googleusercontent.com', 'VxtlR9ypcVt75KMinPUZpbEP', skip_jwt: true
+  # provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"],ENV["GOOGLE_CLIENT_SECRET"], skip_jwt: true
 end
